8d862b04f200c03ca24da3df8aea25ac7a29f3f3
Maksad Khalipaev <apple@appleui-MacBookAir.local>
2024-06-08 19:07:26 +0500

    deleted non-used files

--- src/common/getCurrentUserId.ts
+++
@@ -1,8 +0,0 @@
-import { JwtService } from "@nestjs/jwt";
-import { Request } from "express";
-
-export function getCurrentUserId(jwtService: JwtService, { headers: { authorization } }: Request) {
-    const token = authorization.replace("Bearer ", "");
-
-    return jwtService.decode(token);
-}
